f6795bdd5d5958174a0f8f7eebb15f818e643054
Zdenek Srejber <zdenek.srejber@thenetw.org>
2024-08-17 17:07:08 +0200

    Created initial conversation. Getting player name and sold goods

--- Scripts/Game_State.gd
+++ Scripts/Game_State.gd
@@ -1,4 +1,7 @@
 extends Node
 
-
 var another_attempt: bool  = false
+
+var player_name: String = "Tom Brady"
+
+var sold_goods: String = "Frozen Pizza"
--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,14 +1,33 @@
-extends Button
+extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	loadFirstDialog()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+	
+func loadFirstDialog() -> void:
+	var resource = preload("res://Dialogs/StartDialog.dialogue")
+	DialogueManager.show_example_dialogue_balloon(resource,	"StartDialog",)
+	
 
-func _on_pressed() -> void:
-	DialogueManager.show_example_dialogue_balloon(load("res://Dialogs/StartDialog.dialogue"),"StartDialog")
+func ask_for_name() -> void:
+	var name_input_dialog = load("res://Scenes/NameInputDialog.tscn").instantiate()
+	get_tree().current_scene.add_child(name_input_dialog)
+	name_input_dialog.popup_centered()
+	await name_input_dialog.confirmed
+	GameState.player_name = name_input_dialog.name_edit.text
+	name_input_dialog.queue_free()
+	
+func ask_for_sold_goods() -> void:
+	var name_input_dialog = load("res://Scenes/TextInputDialog.tscn").instantiate()
+	get_tree().current_scene.add_child(name_input_dialog)
+	name_input_dialog.popup_centered()
+	await name_input_dialog.confirmed
+	GameState.sold_goods = name_input_dialog.name_edit.text
+	name_input_dialog.queue_free()
