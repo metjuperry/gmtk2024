7a73d398e64cfc4d886eca26a419a6f0008012be
Zdenek Srejber <zdenek.srejber@thenetw.org>
2024-08-17 22:56:41 +0200

    sidepanel added top 3 random event from json

--- Scripts/V_Box_Side_Menu.gd
+++ Scripts/V_Box_Side_Menu.gd
@@ -20,17 +20,35 @@ func loadEvents() -> void:
 	pass
 	
 func renderButtons() -> void:
-	pass
-	for event in eventData["Events"]["Workshop"]:
-		var containerH = HBoxContainer.new()
+	var array: Array = eventData["Events"]["Workshop"]
+	array.shuffle()
+	for event in array.slice(0,3):
 		var newButton = Button.new()
 		newButton.text = event["Name"]
 		newButton.alignment = HORIZONTAL_ALIGNMENT_LEFT
-		containerH.add_child(newButton)
-		var newLabel = Label.new()
-		newLabel.text = "Aqusition Cost: " + str(event["Aquisition_Cost"])
-		containerH.add_child(newLabel)
-		add_child(containerH)
+		var newAcquisitionLabel = Label.new()
+		newAcquisitionLabel.text = "    Aqusition Cost: " + str(event["Aquisition_Cost"])
+		newAcquisitionLabel.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		newAcquisitionLabel.add_theme_color_override("font_color",getColor(event["Aquisition_Cost"]))
+		var newDailyCostLabel = Label.new()
+		newDailyCostLabel.text = "    Daily Cost: " + str(event["Cost_Per_Cycle"])
+		newDailyCostLabel.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		newDailyCostLabel.add_theme_color_override("font_color",getColor(event["Cost_Per_Cycle"]))
+		var newproductionChangeLabel = Label.new()
+		newproductionChangeLabel.text = "    Daily Production Change: " + str(event["Cost_Per_Cycle"])
+		newproductionChangeLabel.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		newproductionChangeLabel.add_theme_color_override("font_color",getColor(event["Cost_Per_Cycle"]))
+		add_child(newButton)
+		add_child(newAcquisitionLabel)
+		add_child(newDailyCostLabel)
+		add_child(newproductionChangeLabel)
 		
 		
 	
+func getColor(number: float) -> Color:
+	if number>0:
+		return Color.html("#39e75f")
+	elif number<0:
+		return Color.html("#ff474c")
+	else:
+		return Color.html("#FFFFFF")
